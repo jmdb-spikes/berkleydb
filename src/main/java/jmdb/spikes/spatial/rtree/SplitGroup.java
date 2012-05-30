b72a1289bf79745e91c18df0a739117b436f79cf
Jim Barritt <jim@planet-ix.com>
2012-05-30 12:12:03 +0100

    [jmdb] Went too far, bringing it back to index entries

--- src/main/java/jmdb/spikes/spatial/rtree/SplitGroup.java
+++ src/main/java/jmdb/spikes/spatial/rtree/SplitGroup.java
@@ -3,7 +3,6 @@ package jmdb.spikes.spatial.rtree;
 import java.util.ArrayList;
 import java.util.List;
 
-import static jmdb.spikes.spatial.rtree.Rectangle.calculateBoundingRectangle;
 
 public class SplitGroup {
 
@@ -20,7 +19,25 @@ public class SplitGroup {
     }
 
     public Rectangle getBoundingRectangle() {
-        return calculateBoundingRectangle(boundingRectangles);
+        return calculateBoundingRectangle(entries.toArray(new IndexEntry[entries.size()]));
+    }
+
+    public static Rectangle calculateBoundingRectangle(IndexEntry ... indexEntries) {
+        float minX = Float.MAX_VALUE;
+        float maxX = Float.MIN_VALUE;
+
+        float minY = Float.MAX_VALUE;
+        float maxY = Float.MIN_VALUE;
+
+        for (IndexEntry e : indexEntries) {
+            Rectangle r = e.rectangle;
+            minX = (r.x1 < minX) ? r.x1 : minX;
+            maxX = (r.x2 > maxX) ? r.x2 : maxX;
+
+            minY = (r.y1 < minY) ? r.y1 : minY;
+            maxY = (r.y2 > maxY) ? r.y2 : maxY;
+        }
+        return new Rectangle(minX, minY, maxX, maxY);
     }
 
 }
